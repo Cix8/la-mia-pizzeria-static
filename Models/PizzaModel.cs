7317c67f54bdcb8698296d0cc04bb7d24019ecc0
Cix8 <francesco.natal_fn@libero.it>
2022-10-03 14:12:08 +0200

    Aggiungere i file di progetto.

---
+++ Models/PizzaModel.cs
@@ -0,0 +1,22 @@
+﻿namespace la_mia_pizzeria_static.Models
+{
+    public class PizzaModel
+    {
+        private static int idCounter = 0;
+
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public string Image { get; private set; }
+        public float Price { get; private set; }
+
+        public PizzaModel(int id, string name, string description, string image, double price)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Image = image;
+            Price = (float)price;
+        }
+    }
+}
